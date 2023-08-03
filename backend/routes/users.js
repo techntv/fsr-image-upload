e3b87060ac35bbdae01c73256cb555ea8d60a21b
Vinh Nguyen <techntvz@gmail.com>
2023-08-03 17:07:49 +0700

    add email confirmation

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,11 @@
 // backend/routes/index.js
 const express = require("express")
 const router = express.Router(); // Ge the router instance of Express
-const { handleSignup } = require("../controllers/user"); // Get all exported functions in the user controller
+const { handleSignup, handleVerifyEmail } = require("../controllers/user"); // Get all exported functions in the user controller
 
 // Map the `signup` request to the signup function
 router.post("/signup", handleSignup);
+router.get("/verify/:confirmationToken", handleVerifyEmail);
+
 
 module.exports = router;
